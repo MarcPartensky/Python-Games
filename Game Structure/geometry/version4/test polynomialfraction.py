9dd3de63d281655fe872153504257fac0567f95f
Marc <marc.partensky@gmail.com>
2019-09-27 13:45:32 +0200

    added sympy test

--- Game Structure/geometry/version4/test polynomialfraction.py
+++ Game Structure/geometry/version4/test polynomialfraction.py
@@ -1,7 +1,13 @@
 from marclib.polynomial import Polynomial,RationalFunction
 from mynewgrapher import Grapher
 from mycontext import Context
+from sympy import init_printing, Symbol, expand
+init_printing()
 
+a = Symbol('a')
+b = Symbol('b')
+c = Symbol('c')
+d = Symbol('d')
 
 if __name__=="__main__":
     p1=Polynomial([0,6,1,6,5])
@@ -11,6 +17,11 @@ if __name__=="__main__":
     p2=Polynomial([0,5,2,1,-2])
     q2=Polynomial([0,6,1,6,5])
     f2=RationalFunction(p2,q2)
+
+    p3=Polynomial([a,b])
+    q3=Polynomial([c,d])
+    f3=RationalFunction(p3,q3)
+    print(f3)
 
     f=f1+f2
 
